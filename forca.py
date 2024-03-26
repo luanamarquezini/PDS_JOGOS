a6c6aaf0a486d149b8be508a023426cc8f3192d6
Luana Marquezini <130071946+luanamarquezini@users.noreply.github.com>
2024-03-26 09:26:46 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,8 @@
+def jogar():
+print("*********************************")
+print("***Bem vindo ao jogo da Forca!***")
+print("*********************************")
+
+palavra_secreta = "maça" . upper()
+letras acertadas = ["_" for letra in palavra_secreta]
+letras acertadas2 = []                 
